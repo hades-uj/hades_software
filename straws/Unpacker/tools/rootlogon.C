4cc9612a610350cba2fe5eaa43d4d934adfab720
Hades user <lab044Wiki@gmail.com>
2012-01-06 01:23:43 +0100

    update

---
+++ straws/Unpacker/tools/rootlogon.C
@@ -0,0 +1,10 @@
+{
+
+gSystem->Load("libGraf");
+gSystem->Load("../Unpacker/libunpacker_wk.so");
+printf("UWAGA: UNPACKER DLA POJEDYNCZEGO TRB\n"); 
+//gROOT->ProcessLine(".L makeTdcHistos.C");
+//	   gROOT->ProcessLine(".L analysis.C");
+//	   gROOT->ProcessLine(".L DriftTime.C");
+//	   gROOT->ProcessLine(".L Planes.C");
+}
